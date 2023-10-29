3d7345170447b581f4f7bc0e77545a4caa690e07
askhuran <avikhurana03@gmail.com>
2023-10-29 18:45:58 -0400

    hi

---
+++ functions.js
@@ -0,0 +1,14 @@
+function myFunction() {
+    if (document.getElementById("Choices").value = "Edit") {
+        window.location.href = 'edit.html';
+        return;
+    }
+    if (document.getElementById("Choices").value = "Add") {
+        window.location.href = 'edit.html';
+        return;
+    }
+    if (document.getElementById("Choices").value = "Delete") {
+        window.location.href = 'edit.html';
+        return;
+    }
+}
