5744411ec830c73efc441e9fa7ab4532ab943455
Zachary Hanson <avikhurana03@gmail.com>
2023-10-30 12:37:49 -0400

    adding add degree functionality

--- functions.js
+++ functions.js
@@ -1,14 +1,16 @@
-function myFunction() {
-    if (document.getElementById("Choices").value = "Edit") {
+function mainSelect() {
+    console.log(document.getElementById("Choices").value)
+    if (document.getElementById("Choices").value == "Edit") {
         window.location.href = 'edit.html';
         return;
     }
-    if (document.getElementById("Choices").value = "Add") {
-        window.location.href = 'edit.html';
+    if (document.getElementById("Choices").value == "Add") {
+        window.location.href = 'add.html';
         return;
     }
-    if (document.getElementById("Choices").value = "Delete") {
-        window.location.href = 'edit.html';
+    if (document.getElementById("Choices").value == "Delete") {
+        window.location.href = 'delete.html';
         return;
     }
 }
+
